736c52615758a8bdefb7ff06e354b38bb8ae18aa
youmin <105027461+futureyp@users.noreply.github.com>
2023-09-22 14:48:13 -0700

    초기 커밋.

---
+++ BulkyWed/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyWed.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
