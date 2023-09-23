812e6b075ed25b1beafb6695dab410f4099337d7
youmin <105027461+futureyp@users.noreply.github.com>
2023-09-23 00:48:29 -0700

    init

---
+++ BulkyWed/Controllers/Categoryontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWed.Controllers
+{
+    public class Categoryontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
