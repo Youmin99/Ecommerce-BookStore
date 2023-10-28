36cd0c5531400cc7fcfb542c886c01e42050da6e
youmin <logic9904@gmail.com>
2023-10-27 23:57:14 -0700

    orderController

---
+++ BulkyWed/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Repository.IRepository;
+using Models;
+using Models.ViewModels;
+using Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Checkout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+		public class OrderController : Controller
+		{
+			private readonly IUnitOfWork _unitOfWork;
+            [BindProperty]
+            public OrderVM OrderVM { get; set; }
+        public OrderController(IUnitOfWork unitOfWork)
+			{
+				_unitOfWork = unitOfWork;
+			}
+
+			public IActionResult Index()
+			{
+				return View();
+			}
+
+        public IActionResult Details(int orderId)
+        {
+            OrderVM = new OrderVM()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == orderId, includeProperties: "Product"),
+            };
+            return View(OrderVM);
+        }
+
+        #region API CALLS
+        [HttpGet]
+			public IActionResult GetAll()
+			{
+				IEnumerable<OrderHeader> orderHeaders;
+				orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+				return Json(new { data = orderHeaders });
+			}
+			#endregion
+		}
+
+}
